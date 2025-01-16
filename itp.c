13576426321b7341953e0ac6e28a4b06cb6990b4
Sumit Vikram <vikramsumit@outlook.com>
2025-01-16 16:39:26 +0530

    today code

--- itp.c
+++ itp.c
@@ -3,82 +3,102 @@
 #include <ctype.h>
 #include <string.h>
 
-#define MAX 100
+#define MAX 534654
 
 char stack[MAX];
 int top = -1;
 
-void push(char ch) {
-    if (top == MAX - 1) {
+void push(char ch)
+{
+    if (top == MAX - 1)
+    {
         printf("Stack Overflow\n");
         return;
     }
     stack[++top] = ch;
 }
 
-char pop() {
-    if (top == -1) {
+char pop()
+{
+    if (top == -1)
+    {
         printf("Stack Underflow\n");
         return '\0';
     }
     return stack[top--];
 }
 
-char peek() {
-    if (top == -1) {
+char peek()
+{
+    if (top == -1)
+    {
         return '\0';
     }
     return stack[top];
 }
 
-int isOperator(char ch) {
+int isOperator(char ch)
+{
     return ch == '+' || ch == '-' || ch == '*' || ch == '/';
 }
 
-int precedence(char ch) {
-    if (ch == '+' || ch == '-') {
+int precedence(char ch)
+{
+    if (ch == '+' || ch == '-')
+    {
         return 1;
     }
-    if (ch == '*' || ch == '/') {
+    if (ch == '*' || ch == '/')
+    {
         return 2;
     }
     return 0;
 }
 
-void infixToPostfix(char infix[], char postfix[]) {
+void infixToPostfix(char infix[], char postfix[])
+{
     int i, j = 0;
 
-    for (i = 0; infix[i] != '\0'; i++) {
+    for (i = 0; infix[i] != '\0'; i++)
+    {
         char ch = infix[i];
 
-        if (isalnum(ch)) {
+        if (isalnum(ch))
+        {
             postfix[j++] = ch;
         }
-        else if (ch == '(') {
+        else if (ch == '(')
+        {
             push(ch);
         }
-        else if (ch == ')') {
-            while (top != -1 && peek() != '(') {
+        else if (ch == ')')
+        {
+            while (top != -1 && peek() != '(')
+            {
                 postfix[j++] = pop();
             }
             pop();
         }
-        else if (isOperator(ch)) {
-            while (top != -1 && precedence(peek()) >= precedence(ch)) {
+        else if (isOperator(ch))
+        {
+            while (top != -1 && precedence(peek()) >= precedence(ch))
+            {
                 postfix[j++] = pop();
             }
             push(ch);
         }
     }
 
-    while (top != -1) {
+    while (top != -1)
+    {
         postfix[j++] = pop();
     }
 
     postfix[j] = '\0';
 }
 
-int main() {
+int main()
+{
     char infix[MAX], postfix[MAX];
 
     printf("Enter an infix expression: ");
